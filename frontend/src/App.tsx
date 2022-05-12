66208ca64313e680adcedbaa8cba31a0f4084be5
Rodrigo Lopes <rodrigo.lopes44@gmail.com>
2022-05-12 20:48:21 -0300

    Routes added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,20 @@
-import Navbar from "./components/navbar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Listing from "pages/Listing";
+import Form from "pages/Form";
+import Navbar from "components/navbar";
 
 function App() {
-    return <Navbar />;
+    return (
+        <BrowserRouter>
+            <Navbar />
+            <Routes>
+                <Route path='/' element={<Listing />}></Route>
+                <Route path='/form'>
+                    <Route path=':movieId' element={<Form />}></Route>
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
